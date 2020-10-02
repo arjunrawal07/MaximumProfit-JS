f1834e031d9dd216b1528c699bfe50ea530dd857
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-10-02 15:50:57 -0400

    used Math min and max to calculate maxProfit between stock prices on a givenday

--- MaximumProfit.js
+++ MaximumProfit.js
@@ -1,7 +1,17 @@
+//let input = [45, 24, 35, 31, 40, 38, 11] ;
+
 class MaximumProfit {
-    find(input){
-        return null;
+  find(input) {
+    let maxProfit = 0;
+    //Start min at first price value to start comparisons
+    let min = input[0];
+    for (let i = 1; i < input.length; i++) {
+      min = Math.min(input[i], min);
+      //Calculate the max value between maxProfit and current delta between prices
+      maxProfit = Math.max(maxProfit, input[i] - min);
     }
+    return maxProfit;
+  }
 }
 
 module.exports = MaximumProfit;
